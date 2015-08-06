3a64f56589b1a2fd526896f2b73cdf41ea7f666b
d <christian.jensen@spaced.se>
2015-08-06 14:34:03 -0700

    updaTE

---
+++ Example/Pods/Headers/Private/SinchCallingUIKit/CallingManager.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/CallingManager.h
---
+++ Example/Pods/Target Support Files/Pods-SinchCallingUIKit_Tests/Pods-SinchCallingUIKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SinchCallingUIKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SinchCallingUIKit_Tests
+@end
